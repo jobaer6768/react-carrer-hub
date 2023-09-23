77bf7009e26a0af58a6d9b23806398619bf84992
Md Jobaer Hossain <120457646+jobaer6768@users.noreply.github.com>
2023-09-23 22:23:13 +0600

    added show all btn

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -5,19 +5,29 @@ import Job from "../Job/Job";
 const FeaturedJobs = () => {
 
     const [jobs, setJobs] = useState([]);
+    const [dataLength, setDataLength] = useState(4);
 
-    useEffect( () => {
+    useEffect(() => {
         fetch('jobs.json')
             .then(res => res.json())
             .then(data => setJobs(data));
     }, [])
 
     return (
-        <div className="grid grid-cols-2 gap-6 mt-8">
-            {
-                jobs.map(job => <Job key={job.id} job={job}></Job>)
-            }
+        <div>
+
+            <div className="grid grid-cols-2 gap-6 mt-8">
+                {
+                    jobs.slice(0, dataLength).map(job => <Job key={job.id} job={job}></Job>)
+                }
+            </div>
+
+            <div className={dataLength === jobs.length ? 'hidden' : 'text-center mt-3'}>
+                <button onClick={() => setDataLength(jobs.length)} className="btn btn-primary text-white">Show All</button>
+            </div>
+
         </div>
+
     );
 };
 
